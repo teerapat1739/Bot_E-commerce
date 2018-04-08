199008047ad1756a2d213981f55db1072fa77b13
teerapat1739 <teerapat12345678@gmail.com>
2018-04-08 22:25:36 +0700

    handle multer module to manage image

--- routes/admin.js
+++ routes/admin.js
@@ -4,7 +4,17 @@ const router = require('express').Router()
 const logger = require('../log')
 const Category = require('../models/category')
 const Product = require('../models/product')
+const multer = require('multer')
+const path = require('path')
+const fs = require('fs')
+const signature = {
+    jpg: 'ffd8ffe0',
+    jpg_exif: 'ffd8ffe1',
+    png: '89504e47',
+    gif: '47494638'
+}
 
+let checkSignature = value => value === signature.jpg || value === signature.jpg_exif || value === signature.gif || value === signature.png ? true : false
 
 router.get('/admin',(req, res) => {
     res.render('admin')
@@ -20,6 +30,45 @@ router.post('/category', (req, res) => {
     })
 })
 
+
+router.post('/product', (req, res) => {
+    let upload = multer({
+        storage: multer.memoryStorage()
+    }).single('image')
+
+    upload(req, res, err => {
+        let buffer = req.file.buffer
+        let sgnt = buffer.toString('hex', 0, 4)
+        console.log(sgnt)
+        let filename = req.file.fieldname + '-' + Date.now() + path.extname(req.file.originalname)        
+        console.log(filename)
+        if(checkSignature(sgnt)) {
+            let product = new Product({
+                category: req.body.ctg,
+                name: req.body.name,
+                price: req.body.price,
+                image: filename,
+                description: req.body.des
+            })
+            product.save().then( data => {
+                fs.writeFile('./public/uploads/'+filename, buffer, 'binary', err => {
+                    if(err) {
+                        Product.findByIdAndRemove(data._id).then(data => {
+                            console.log(data)
+                        }, (e) => {
+                            console.log('Unable to remove from db', e)
+                            
+                        })
+                        throw new Error('Unable to write file!')
+                    }
+                    res.render('admin',{msg: 'File upload successfully!'})
+                })
+            },(e)=> {throw new Error('Unable to save to DB')})
+        }else {
+            res.render('admin',{msg:' Remember: Only images!'})
+        }
+    })
+})
 
 //upload file will use Multer module 
 
