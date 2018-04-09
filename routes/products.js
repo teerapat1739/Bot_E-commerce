c6b9e4eebafa05c2f122333bcfe29015059b27c2
teerapat1739 <teerapat12345678@gmail.com>
2018-04-09 22:31:41 +0700

    complete pagination page

--- routes/products.js
+++ routes/products.js
@@ -7,4 +7,23 @@ const Product = require('../models/product')
 
 router.get('/navbar', (req, res) => {
     const perPage = 8
+    const page = req.query.page || 1
+    const ctg = req.query.name
+
+    Product.find({category: req.query.name})
+        .skip((perPage * page) - perPage)
+        .limit(perPage)
+        .then(products => {
+            Product.count({category: req.query.name})
+                .then(count => {
+                    res.render('product', {
+                        navbar: products,
+                        current: page,
+                        pages: Math.ceil(count/perPage),
+                        category:ctg
+                    })
+                }, (e) => {throw new Error('Unable to count!')})
+        }, (e) => {throw new Error('Unable to find this product!')})
 })
+
+module.exports = router
